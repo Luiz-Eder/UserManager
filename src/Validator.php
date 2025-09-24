933639889ea8c9b285fd2384b77d749b67aa1676
Eder Luiz <160525399+Luiz-Eder@users.noreply.github.com>
2025-09-23 21:46:41 -0300

    Update Validator.php

--- src/Validator.php
+++ src/Validator.php
@@ -9,10 +9,17 @@ class Validator
         return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
 
-    public function validatePasswordStrength(string $password): bool
+    public function validatePasswordStrength(string $password): string
     {
-        return strlen($password) >= 8
-            && preg_match('/[0-9]/', $password)
-            && preg_match('/[A-Z]/', $password);
+        if (strlen($password) < 8) {
+            return "Erro: Sua senha deve ter pelo menos 8 caracteres";
+        }
+        if (!preg_match('/[0-9]/', $password)) {
+            return "Erro: Sua senha deve ter pelo menos um numero";
+        }
+        if (!preg_match('/[A-Z]/', $password)) {
+            return "Erro: Sua senha deve ter pelo menos uma letra maiuscula";
+        }
+        return "";
     }
 }
